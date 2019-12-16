d5a5fef329bc6945bb7ca17a314ae74137293cb9
gyqls <gyqls1494@gmail.com>
2019-12-16 22:11:55 +0900

    IO

---
+++ hyobin/Basic_Algorithm_1/Scanner_.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Scanner_{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);  
+		int count = scanner.nextInt();
+		int a, b;
+		for(int i =0; i<count; i++) {
+			a = scanner.nextInt();
+			b = scanner.nextInt();
+			System.out.println(a+b);
+		}
+	}
+}
