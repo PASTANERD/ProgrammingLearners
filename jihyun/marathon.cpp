4758fa79918c7ff1e8e689f18e9e7826adfe5ae8
Jihyun <21500376@handong.edu>
2019-12-16 19:20:57 +0900

    test

---
+++ jihyun/marathon.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    
+    int i = 0;
+    
+    for(;i < completion.size(); i++){
+        if(participant[i] !=completion[i])
+            break;
+    }
+    
+    return participant[i];
+    
+}
