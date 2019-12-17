3a3ed620c7741ec3c613eaefe1f01b8379ad5308
gyqls <gyqls1494@gmail.com>
2019-12-17 12:23:31 +0900

    10809

--- hyobin/Basic_Algorithm_1/BufferedReader_.java
+++ hyobin/Basic_Algorithm_1/BufferedReader_.java
@@ -1,4 +1,21 @@
+import java.io.*;
+import java.util.*;
 
-public class BufferedReader_ {
-
-}
+public class BufferedReader_{
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader (new InputStreamReader (System.in));
+		String s = bf.readLine(); //String
+		int i = Integer.parseInt(bf.readLine()); //Int
+		
+		StringTokenizer st = new StringTokenizer(s);
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		String array[] = s.split(" ");
+		
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+		bw.write(s+"\n");
+		bw.flush();
+		bw.close();
+	}
+}   
