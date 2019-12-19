4417c3b4d848c4d2c9b0c644f691a2845deb088b
Jihyun <21500376@handong.edu>
2019-12-19 14:01:52 +0900

    ds

--- jihyun/DAY3_DP/to1(1463).cpp
+++ jihyun/DAY3_DP/to1(1463).cpp
@@ -35,35 +35,4 @@ int main(){
     
     cout << sub(n);
 }
-//input : 첫째 줄에 n이 주어진다. (1 ≤ n ≤ 1,000)
-//output : 첫째 줄에 2×n 크기의 직사각형을 채우는 방법의 수를 10,007로 나눈 나머지를 출력한다.
 
-#include <iostream>
-
-using namespace std;
-
-int d[1000] = {0};
-
-int sub(int n){
-    if(n == 1)
-        return 1;
-    if(n == 2)
-        return 2;
-    
-    if(d[n] > 0)
-        return d[n];
-    
-    d[n] = sub(n-2) + sub(n-1);
-    return d[n];
-}
-
-int main(){
-    
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int n;
-    cin >> n;
-    
-    cout << sub(n)%10007;
-}
