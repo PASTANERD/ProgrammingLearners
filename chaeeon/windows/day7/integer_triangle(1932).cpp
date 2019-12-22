fa1999ce49be3b7fef3ba49f4bbfc54a884fbf57
PASTANERD <pastanerd0416@gmail.com>
2019-12-23 01:42:44 +0900

    day7

---
+++ chaeeon/windows/day7/integer_triangle(1932).cpp
@@ -0,0 +1,41 @@
+/*
+*   Baekjoon Online Judge
+*   https://www.acmicpc.net/problem/1932
+*   
+*   Solved by chaeeon lim
+*   pastanerd0416@gmail.com
+*/
+
+/*
+ *  best(N):    best cases to have maximum wines for number of N wines
+ *  best(N) = max(cases[N][0], cases[N][1], cases[N][2]) 
+ *  cases[N][C]: 
+ *  cases[N][0] = max(cases[N-1], cases[N-1][1], cases[N-1][2])
+ *  cases[N][1] = cases[N-1][0] + wine[N]
+ *  cases[N][2] = cases[N-1][1] + wine[N]
+ *  
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+    cin.ignore();
+
+    int *triangle = new int[(N+1)*N/2];
+
+    for(int i = 0 ; i < (N+1)*N/2 ; i++){
+        cin >> triangle[i];
+        cout << triangle[i] << ' ';
+    }
+
+
+    return 0;
+}
