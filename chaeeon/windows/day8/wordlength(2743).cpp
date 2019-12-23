551ff4d0e1b414de29fcf8d0b2a46da634af948c
PASTANERD <pastanerd0416@gmail.com>
2019-12-24 02:35:41 +0900

    day8

---
+++ chaeeon/windows/day8/wordlength(2743).cpp
@@ -0,0 +1,24 @@
+/*
+*   Baekjoon Online Judge
+*   https://www.acmicpc.net/problem/2743
+*   
+*   Solved by chaeeon lim
+*   pastanerd0416@gmail.com
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string N;
+    
+    cin >> N;
+    cout << N.length();
+
+    return 0;
+}
