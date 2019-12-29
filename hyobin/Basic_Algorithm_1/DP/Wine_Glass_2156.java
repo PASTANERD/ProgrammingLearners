2dcddec4ba20b301b030683a7273ec9411527510
gyqls <gyqls1494@gmail.com>
2019-12-29 21:37:03 +0900

    2156

--- hyobin/Basic_Algorithm_1/DP/Wine_Glass_2156.java
+++ hyobin/Basic_Algorithm_1/DP/Wine_Glass_2156.java
@@ -8,21 +8,21 @@ public class Wine_Glass_2156 {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		int N = scanner.nextInt();
-		int[] D = new int[N];
-		int[] wine = new int[N];
+		int max = 10000;
+		int[] D = new int[max+1];
+		int[] wine = new int[max+1];
 		
 		
-		for(int i=0; i<N; i++) {
+		for(int i=0; i<N; i++) 
 			wine[i] = scanner.nextInt();
-		}
 		
+		//이경우에, N만큼 배열선언했으면 1<=n<=10000 이므로 n이 1or2이면 에러남.
 		D[0] = wine[0];
 		D[1] = wine[0]+wine[1];
 		D[2] = MAX(D[1], wine[0]+wine[2], wine[1]+wine[2]);
 		
-		for(int i=3; i<N; i++) {
+		for(int i=3; i<N; i++) 
 			D[i] = MAX(D[i-1], D[i-2] + wine[i], D[i-3]+wine[i-1]+wine[i]);
-		}
 		
 		System.out.println(D[N-1]);
 
@@ -30,7 +30,7 @@ public class Wine_Glass_2156 {
 	
 	public static int MAX(int a, int b, int c) {
 		int max = a;
-		if(b> max) max = b;
+		if(b > max) max = b;
 		if(c > max) max = c;
 		return max;
 	}
