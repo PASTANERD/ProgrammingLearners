ca3e69f58d4ed9ed921afefa43c1ac3efa5584c9
ChaeEon Lim <pastanerd0416@gmail.com>
2019-12-31 11:59:34 +0900

    day12

--- chaeeon/day12/date_calculation(1476).cpp
+++ chaeeon/day12/date_calculation(1476).cpp
@@ -22,9 +22,7 @@ int main(){
     int e = 1, s = 1, m = 1;
     int years = 1;
     while(true){
-        if(e == E && s == S && m == M){
-            break;
-        }
+        if(e == E && s == S && m == M) break;
         e += 1;
         s += 1;
         m += 1;
