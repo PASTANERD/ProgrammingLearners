5baac794d2976d0ccb986b594b30e6aaf8f2c248
ccssong <21500747@handong.edu>
2020-01-02 04:12:27 +0900

    day5

---
+++ eunsong/day4/tetromino(14500).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+/**
+ * 나중에 풀어보기~!!!
+ * Gold
+ * 
+*/
+
+    return 0;
+}
---
+++ eunsong/day5/prevpermutation(10973).cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int findnext(int *a, int n){
+    int next=0;
+    for(int i=1; i<n; i++){
+        if(a[i-1] > a[i])
+            next = i;
+    }
+    return next;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    int *a = new int[n];
+    for(int i=0; i<n; i++)
+        cin >> a[i];
+
+   int next = findnext(a, n);
+  //  cout << next << endl;
+
+    if(next == 0){
+        next = -1;
+        cout << next << "\n";
+        return 0;
+    }
+
+    int j=next;
+    for(int i=next; i<n; i++){
+        if(a[next-1] > a[i])
+            j = (a[i] > a[j]) ? i : j;
+    } 
+    
+    swap(a[next-1], a[j]);
+
+    for(int i=n-1; i>next; i--){
+        for(int k=next; k<=i; k++)
+            if(a[k] < a[k+1])
+                swap(a[k],a[k+1]);
+    }
+
+    for(int i=0; i<n; i++)
+        cout << a[i] << " ";
+    cout << "\n";
+
+    return 0;
+}
