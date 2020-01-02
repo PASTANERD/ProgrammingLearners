063d286ff59aba53346cab81add38d5d094f020f
gyqls <gyqls1494@gmail.com>
2020-01-02 15:47:33 +0900

    15652

---
+++ hyobin/Basic_Algorithm_2/NnM/NnM_3_15651.java
@@ -0,0 +1,31 @@
+package NnM;
+
+
+import java.io.*;
+import java.util.*;
+
+public class NnM_3_15651 {
+	
+	public static int N, M;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(stk.nextToken());
+		M = Integer.parseInt(stk.nextToken());
+		
+		go(0 , "");
+		System.out.println(sb);
+	}
+	
+	public static void go(int index, String s) throws IOException {
+		if(index == M) {
+			sb.append(s + "\n");
+			return;
+		}
+		for(int i=1; i<=N; i++) go(index+1, s + i + " ");
+	}
+}
