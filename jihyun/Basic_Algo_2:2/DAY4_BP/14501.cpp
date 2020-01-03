0ed7b2218ea63cd21ce6beaa7f176a335e1b9fb5
Jihyun <21500376@handong.edu>
2020-01-03 11:02:18 +0900

    0102

---
+++ jihyun/Basic_Algo_2:2/DAY4_BP/14501.cpp
@@ -0,0 +1,37 @@
+//1759
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int schedule[16][2];
+int ans = 0;
+int n;
+
+void check(int day, int sum){
+    if(day == n+1){
+       if(sum > ans) ans = sum;
+       return;
+    }
+    if(day>n+1)
+        return
+    
+    check(day+1, sum);
+    check(day+schedule[day][0], day+schedule[day][1]);
+    
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    cin >> n;
+
+    for(int i=1; i<=n; i++)
+        cin >> schedule[i][0] >> schedule[i][1];
+    
+    check(1,0);
+    cout << ans << '\n';
+}
