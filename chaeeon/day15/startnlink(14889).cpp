761368914ee67b0a899aa05e2c8598c8961faf59
ChaeEon Lim <pastanerd0416@gmail.com>
2020-01-03 20:26:03 +0900

    day16

--- chaeeon/day15/startnlink(14889).cpp
+++ chaeeon/day15/startnlink(14889).cpp
@@ -12,26 +12,12 @@
 using namespace std;
 
 
-int N;
-int *T, *P;
-
-int make_money(int sum, int day){
-    if(day == N) return sum;
-    if(day > N) return 0;
-    else return max(make_money(sum+P[day], day+T[day]), make_money(sum, day+1));
-}
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int N;
     cin >> N;
-
-    T = new int[N];
-    P = new int[N];
-    for(int i = 0 ; i < N ; i++) cin >> T[i] >> P[i];
-    
-    cout << make_money(0, 0);
 
     return 0;
 }
