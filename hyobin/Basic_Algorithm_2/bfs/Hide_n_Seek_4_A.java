9dc5114d7c7fee2868af72100529fa90d4e9df77
gyqls <gyqls1494@gmail.com>
2020-01-09 18:10:32 +0900

    findseek4

--- hyobin/Basic_Algorithm_2/bfs/Hide_n_Seek_4_A.java
+++ hyobin/Basic_Algorithm_2/bfs/Hide_n_Seek_4_A.java
@@ -7,7 +7,7 @@ public class Hide_n_Seek_4_A {
 		int index;
 		int loc;
 		
-		Item(int idex, int loc){
+		Item(int index, int loc){
 			this.index = index;
 			this.loc = loc;
 		}
@@ -16,29 +16,58 @@ public class Hide_n_Seek_4_A {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer tokenizer = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(tokenizer.nextToken());
         int k = Integer.parseInt(tokenizer.nextToken());
         
         if(n >= k) {
-        	bw.write(String.valueOf(n-k));
-        	bw.write("\n");
+        	sb.append(String.valueOf(n-k));
+        	sb.append("\n");
         	for(int i=n ; i>=k; --i) {
-        		bw.write(String.valueOf(i));
-        		bw.write(" ");
+        		sb.append(String.valueOf(i));
+        		sb.append(" ");
         	}
         } else {
         	Queue<Item> queue = new LinkedList<>();
         	queue.add(new Item(0, n)); //n부터 시작
-        	int[] parent = new int[200001];
+        	//index가 0부터이기 때문에, 0이랑 구분해야함
+        	//그래서 Integer[] 는 기본값 null이어서, 0이랑 구분됨
+        	Integer[] parent = new Integer[200001];
         	
-        	Item result;
+        	Item result = null;
         	while(!queue.isEmpty()) {
+        		
         		result = queue.poll();
-        		int loc = result.loc;
+        		int loc = result.loc;     //n
+        		if(loc == k) {
+        			break;
+        		}
+        		if((loc-1) > 0 && parent[loc-1] == null) {
+        			queue.add(new Item(result.index+1, loc-1));
+        			parent[loc-1] = loc;
+        		}
+        		if(loc+1 < 200000 && parent[loc+1]== null) {
+        			queue.add(new Item(result.index+1, loc+1));
+        			parent[loc+1] = loc;
+        		}
+        		if(loc*2 < 200000 && parent[loc*2] == null) {
+        			queue.add(new Item(result.index+1, loc*2));
+        			parent[loc*2] = loc;
+        		}
         	}
+        	System.out.println(result.index);
+        	recursive(sb, parent, result.loc, n);
         }
+        System.out.print(sb);
+        br.close();
 	}
-
+	
+	private static void recursive(StringBuilder sb, Integer[] par, int loc, int first) throws IOException { 
+		sb.insert(0, String.valueOf(loc) + " ");
+		if(loc==first) {
+			return;
+		}
+		recursive(sb, par, par[loc], first);
+	}
 }
