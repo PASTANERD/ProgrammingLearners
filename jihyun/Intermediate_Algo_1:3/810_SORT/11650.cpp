cab7cb73b6978d5865757bc9131fb35b7e8b7686
Jihyun <21500376@handong.edu>
2020-03-13 00:50:58 +0900

    jh

--- jihyun/Intermediate_Algo_1:3/810_SORT/11650.cpp
+++ jihyun/Intermediate_Algo_1:3/810_SORT/11650.cpp
@@ -1,14 +1,10 @@
+
 //11650
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
-
-bool cmp(pair<int,int> a, pair<int,int> b){
-    if(a.first < b.first) return a.first < b.first;
-    else return a.second < b.second;
-}
 
 int main(){
     int n;
@@ -18,12 +14,12 @@ int main(){
     for(int i=0; i<n; i++){
         int x, y;
         cin >> x >> y;
-        v.push_back({x,y});
+        v.push_back(make_pair(x,y));
     }
+
+    sort(v.begin(),v.end());
     
-    sort(v.begin(),v.end(),cmp);
-    
-    for(pair<int,int> p : v)
+    for(auto p : v)
         cout << p.first << ' ' << p.second << '\n';
 }
 
--- jihyun/Intermediate_Algo_1:3/810_SORT/11651.cpp
+++ jihyun/Intermediate_Algo_1:3/810_SORT/11651.cpp
@@ -1,4 +1,3 @@
-
 //11651
 #include <iostream>
 #include <vector>
@@ -8,22 +7,26 @@ using namespace std;
 
 bool cmp(pair<int,int> a, pair<int,int> b){
     if(a.second < b.second) return true;
-    else return a.first < b.first;
+    if(a.second > b.second) return false;
+    return (a.first < b.first);
+    
 }
 
 int main(){
     int n;
     cin >> n;
-    vector<pair<int,int>> v;
+    vector< pair<int,int> > v;
     
     for(int i=0; i<n; i++){
         int x, y;
         cin >> x >> y;
-        v.push_back(make_pair(x,y));
+        v.push_back({x,y});
     }
-    
+
     sort(v.begin(),v.end(),cmp);
     
-    for(pair<int,int> p : v)
+    for(auto p : v)
         cout << p.first << ' ' << p.second << '\n';
 }
+
+
